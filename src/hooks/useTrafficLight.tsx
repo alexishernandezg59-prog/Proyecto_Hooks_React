3e7bdac3eec205b05a6f83f6e116d836258d7b5c
Alexis <alexishernandezg59@gmail.com>
2026-03-29 19:24:15 -0600

    feat: initial commit

---
+++ src/hooks/useTrafficLight.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+    blue: 'bg-blue-500 animate-pulse',
+};
+
+type TrafficLightColor = keyof typeof colors;
+
+export const useTrafficLight = () => {
+
+    const[light, setlight] = useState<TrafficLightColor>('red');
+    const[countdown, setCountdown] = useState(5);
+
+    //Countdown effect
+    useEffect(() => {
+      if(countdown ==0) return;
+
+      const intervalId = setInterval(()=> {
+        console.log('setInterval llamado')
+        setCountdown(prev => prev - 1)
+      }, 1000)
+
+      return() => {
+        console.log('Cleanup effect')
+        clearInterval(intervalId);
+      }
+
+    }, [countdown])
+
+
+    //Change light color effect
+    useEffect(() => {
+      if(countdown > 0) return;
+      setCountdown(5);
+      if(light == 'red'){
+        setlight('green');
+        return;
+      }
+      if(light =='yellow'){
+        setlight('red');
+        return;
+      }
+      if(light == 'green'){
+        setlight('yellow');
+        return;
+      }
+    }, [countdown,light])
+
+  return {
+    //Properties
+    countdown,
+    light,
+    colors,
+
+    //Computed
+    percentage: countdown/5 * 100,
+    greenLight: light == 'green' ? colors.green : 'bg-gray-500',
+    redLight: light == 'red' ? colors.red : 'bg-gray-500',
+    yellowLight: light == 'yellow' ? colors.yellow : 'bg-gray-500',
+
+    //Methods
+  };
+}
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import { TrafficLight } from './02-useEffect/TrafficLightWithEffect'
+//import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
+//import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
+//import { TrafficLight } from './01-useState/TrafficLight'
+//import { HooksApp } from './HooksApp'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <FocusScreen/>
+  </StrictMode>,
+)
