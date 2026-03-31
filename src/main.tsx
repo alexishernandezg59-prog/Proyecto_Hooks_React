b85bac98dad26f09f541f170cf4c04a5cdd70b2d
Alexis <alexishernandezg59@gmail.com>
2026-03-31 11:26:59 -0600

    Primer commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import './index.css'
 //import { TrafficLight } from './02-useEffect/TrafficLightWithEffect'
 //import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 //import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './04-useRef/FocusScreen'
+//import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 //import { TrafficLight } from './01-useState/TrafficLight'
 //import { HooksApp } from './HooksApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FocusScreen/>
+    <TasksApp/>
   </StrictMode>,
 )
